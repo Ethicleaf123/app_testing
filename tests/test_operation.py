e04d2be21af3108c321f07ea9abf48e82f68788a
Ethicleaf123 <ravikant.ethicleaf@gmail.com>
2026-04-02 12:13:31 -0400

    resolve type in test_operation

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(-1, 1) == 0
 
 def test_sub():
-    assert sub(10, 5) == 5
-    assert sub(5, 10) == -5
-    assert sub(4, 3) == 1
-    assert sub(2, 3) == -1
+    assert subtract(10, 5) == 5
+    assert subtract(5, 10) == -5
+    assert subtract(4, 3) == 1
+    assert subtract(2, 3) == -1
