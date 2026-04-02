428d7c2d5ab45ce4b3ed633363ec4a2a7c5f139a
Ethicleaf123 <ravikant.ethicleaf@gmail.com>
2026-04-02 12:03:49 -0400

    fix the import type in test_operation.py

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(10, 5) == 15
+    assert add(-1, 1) == 0
+
+def test_sub():
+    assert sub(10, 5) == 5
+    assert sub(5, 10) == -5
+    assert sub(4, 3) == 1
+    assert sub(2, 3) == -1
